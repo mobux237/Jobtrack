c080e5d76076af5423ad81af736cfce58a4d2bcf
mobux237 <claudebonny1998@gmail.com>
2026-04-01 20:56:17 +0200

    adds news

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import offres, candidatures, lettres, cv
-from routers.lettres_ats import router as lettres_ats_router
-from routers.cv_ats import router as cv_ats_router
-app.include_router(lettres_ats_router)
-app.include_router(cv_ats_router)
+from app.routes.cv_ats import router as cv_ats_router
+from app.routes.lettres_ats import router as lettres_ats_router
 
 app = FastAPI(title="JobTrack API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:3000",
-    "https://jobtrack-blue-six.vercel.app"],
+        "http://localhost:3000",
+        "https://jobtrack-blue-six.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -22,6 +21,8 @@ app.include_router(offres.router)
 app.include_router(candidatures.router)
 app.include_router(lettres.router)
 app.include_router(cv.router)
+app.include_router(cv_ats_router)
+app.include_router(lettres_ats_router)
 
 @app.get("/")
 def read_root():
